a4523cb8aa7ac2c5183c6ad4931b38114281d20f
Ubuntu <team-papaya@cs224n-dev.31ytmlyni1fu5lufdv1wi1khrg.jx.internal.cloudapp.net>
2018-03-15 19:37:04 +0000

    Hyperparameters update

--- code/main.py
+++ code/main.py
@@ -44,12 +44,12 @@ tf.app.flags.DEFINE_string("experiment_name", "", "Unique name for your experime
 tf.app.flags.DEFINE_integer("num_epochs", 0, "Number of epochs to train. 0 means train indefinitely")
 
 # Hyperparameters
-tf.app.flags.DEFINE_float("learning_rate", 0.05, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate", 0.0001, "Learning rate.")
 tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm.")
 tf.app.flags.DEFINE_float("dropout", 0.20, "Fraction of units randomly dropped on non-recurrent connections.")
-tf.app.flags.DEFINE_float("l2_beta", 0.00001, "L2 regularization parameter.")
+tf.app.flags.DEFINE_float("l2_beta", 0.01, "L2 regularization parameter.")
 tf.app.flags.DEFINE_integer("batch_size", 100, "Batch size to use")
-tf.app.flags.DEFINE_integer("hidden_size", 125, "Size of the hidden states")
+tf.app.flags.DEFINE_integer("hidden_size", 150, "Size of the hidden states")
 tf.app.flags.DEFINE_integer("context_len", 400, "The maximum context length of your model")
 tf.app.flags.DEFINE_integer("question_len", 30, "The maximum question length of your model")
 tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained word vectors. This needs to be one of the available GloVe dimensions: 50/100/200/300")
